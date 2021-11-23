7d6da4a0d47e1425281053392327b50fa5bebfdb
Rob Vanarsdall <48535209+rvanarsdall@users.noreply.github.com>
2021-11-22 20:53:12 -0500

    Update cors.js

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,6 +1,7 @@
 const CorsMiddleware = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "POST, PUT, GET, DELETE, OPTIONS");
+ res.header("access-control-allow-origin", "*");
+  res.header("access-control-allow-methods", "GET, POST, PUT, DELETE");
+  res.header("access-control-allow-headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   return next();
 };
 
